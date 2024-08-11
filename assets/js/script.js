65c2f9259a4b1681d130143941af3c18cd2213c8
HaidarESBER <haidaresber01@gmail.com>
2024-08-11 14:59:06 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 function sendMail(event) {
-    
+
     event.preventDefault(); // Prevent the form from submitting traditionally
     var submitButton = document.getElementById("submit");
 
      // Simple JavaScript validation
      var name = document.getElementById("contact-name").value.trim();
      var email = document.getElementById("contact-email").value.trim();
-     var subject = document.getElementById("subject").value.trim();
      var message = document.getElementById("contact-message").value.trim();
  
-     if (!name || !email || !subject || !message) {
+     if (!name || !email || !message) {
          alert('Please fill in all required fields.');
+         submitButton.disabled = false;
          return; // Stop the form submission
      }
+     submitButton.disabled = false;
+     
     var params = {
         from_name: document.getElementById("contact-name").value,
         phone_number: document.getElementById("contact-phone").value,
